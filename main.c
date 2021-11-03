fe9d32b084358783e40b34df1d5bae83c9d1d4d9
Tyhjakuori <88154596+Tyhjakuori@users.noreply.github.com>
2021-11-03 17:26:47 +0200

    Added database searching function

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 #include <ctype.h>
 #include <sqlite3.h>
 
@@ -8,6 +9,7 @@
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName);
 double haetupla(double *summa); 
+void searchdb();
 sqlite3 *db;
 
 int main() {
@@ -81,18 +83,14 @@ int main() {
 	sqlite3_finalize(stmt);
 
 	printf("Successfully inserted data into the table\n");
-	
-	rc = sqlite3_exec(db, "SELECT * FROM expenses;", callback, 0, &errmsg);
-	if (rc != SQLITE_OK) {
-		fprintf(stderr, "Can't select data from the table: %s\n", sqlite3_errmsg(db));
-		sqlite3_close(db);
-		return(1);
-	}
 	sqlite3_close(db);
+	free(ptr);
+	searchdb();
 	return 0;
 }
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+	
 	int i;
 	for(i=0; i<argc; i++) {
 		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
@@ -102,10 +100,117 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
 }
 
 double haetupla(double *summa) {
+	
 	char s[20];
 
 	printf("Summa: ");
 	fgets(s, sizeof(s), stdin);
 	sscanf(s, "%lf", *&summa);
 	return *summa;
+}
+
+void searchdb() {
+
+	unsigned int len_max = 200;
+	int rc, size1, temp, answer, sscanf_result;
+	char line[4];
+	char *errmsg = 0;
+	char *naytacolums, *marks[4], *statement = malloc(len_max), *hakusana = malloc(len_max);
+
+	rc = sqlite3_open("./testi.db", &db);
+	if (rc != SQLITE_OK) {
+		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+		sqlite3_close(db);
+	}
+	printf("|||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+	printf("CHOOSE WHICH COLUMN YOU WANT TO USE FOR THE SEARCH\n\n");
+	naytacolums = "pragma table_info(expenses);";
+	rc = sqlite3_exec(db, naytacolums, callback, 0, &errmsg);
+	if (rc != SQLITE_OK) {
+		fprintf(stderr, "Can't select data from the table: %s\n", sqlite3_errmsg(db));
+		sqlite3_close(db);
+	}
+	
+	answer = -1;
+	while (answer != 0) {
+		printf("[1] ID\n");
+		printf("[2] Date\n");
+		printf("[3] Amount\n");
+		printf("[4] Description\n");
+		printf("[5] Print the whole database\n");
+		fgets (line, sizeof(line), stdin);
+		sscanf_result = sscanf(line, "%d", &answer);
+		if ((sscanf_result == 0) | (sscanf_result == EOF)) {
+			printf("Answer needs to be a number\n");
+			answer = -1;
+			break;
+		}
+		switch (answer) {
+			case 0:
+				break;
+			case 1:
+				printf("Hakusana: ");
+				fgets(hakusana, sizeof(hakusana), stdin);
+				hakusana[strcspn(hakusana, "\n")] = 0;
+				temp = sizeof(hakusana);
+				size1 = temp + 45;
+				strcpy(marks, ";");
+				strcpy(statement, "SELECT * FROM expenses WHERE id=");
+				strncat(statement, hakusana, size1);
+				strncat(statement, marks, sizeof(statement+2));
+				answer = 0;
+				break;
+			case 2:
+				printf("Hakusana: ");
+				fgets(hakusana, sizeof(hakusana), stdin);
+				hakusana[strcspn(hakusana, "\n")] = 0;
+				temp = sizeof(hakusana);
+				size1 = temp + 45;
+				strcpy(marks, "%';");
+				strcpy(statement, "SELECT * FROM expenses WHERE date LIKE '%");
+				strncat(statement, hakusana, size1);
+				strncat(statement, marks, sizeof(statement+4));
+				answer = 0;
+				break;
+			case 3:
+				printf("Hakusana: ");
+				fgets(hakusana, sizeof(hakusana), stdin);
+				hakusana[strcspn(hakusana, "\n")] = 0;
+				temp = sizeof(hakusana);
+				size1 = temp + 45;
+				strcpy(marks, "%';");
+				strcpy(statement, "SELECT * FROM expenses WHERE amount LIKE '%");
+				strncat(statement, hakusana, size1);
+				strncat(statement, marks, sizeof(statement+4));
+				answer = 0;
+				break;
+			case 4:
+				printf("Hakusana: ");
+				fgets(hakusana, sizeof(hakusana), stdin);
+				hakusana[strcspn(hakusana, "\n")] = 0;
+				temp = sizeof(hakusana);
+				size1 = temp + 45;
+				strcpy(marks, "%';");
+				strcpy(statement, "SELECT * FROM expenses WHERE desc LIKE '%");
+				strncat(statement, hakusana, size1);
+				strncat(statement, marks, sizeof(statement+4));
+				answer = 0;
+				break;
+			case 5:
+				strcpy(statement, "SELECT * FROM expenses;");
+				answer = 0;
+				break;
+			default:
+				break;
+		}
+	}
+	printf("\n|||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+	printf("SEARCH RESULTS\n\n");
+	rc = sqlite3_exec(db, statement, callback, 0, &errmsg);
+	if (rc != SQLITE_OK) {
+		fprintf(stderr, "Can't select data from the table: %s\n", sqlite3_errmsg(db));
+		sqlite3_close(db);
+	}
+	free(statement);
+	free(hakusana);
 }
